34eca158f4028597e282a2716586ce233a4c07d1
Hypnosss <398906857@qq.com>
2019-07-10 20:11:55 +0800

    lodash-chunk

--- miao/lodash/hypnosss-lodash.js
+++ miao/lodash/hypnosss-lodash.js
@@ -1,5 +1,17 @@
 var hypnosss = {
-	compact: function (ary) {
-		return ary.filter(it => it)
+	chunk: function(arr, n) {
+		var ans = [];
+		var num = Math.ceil(arr.length / n);
+		for(let i = 0; i < num; i++) {
+			if(i == num - 1) {
+				ans[i] = arr.slice((num - 1) * n);
+			} else {
+				ans[i] = arr.slice(i * n, (i + 1) * n);
+			}
+		}
+		return ans;
 	},
+	compact: function (ary) {
+		return ary.filter(it => it);
+	}	
 }
