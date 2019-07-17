c4816a3152e494dd1e7943d3bcf07f34cc9e0a28
Hypnosss <398906857@qq.com>
2019-07-17 16:48:03 +0800

    some functions

--- miao/lodash/hypnosss-lodash.js
+++ miao/lodash/hypnosss-lodash.js
@@ -443,6 +443,7 @@ var hypnosss = {
     return typeof(val) == "function";
   },
   isNaN: function(val) {
+    console.log(val, typeof(val), instanceOf(val))
     return val.toString() === "NaN" || +val.toString() === "NaN";
   },
   isNil: function(val) {
