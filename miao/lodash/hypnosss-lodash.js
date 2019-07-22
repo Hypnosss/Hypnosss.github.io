5c6ce005ad545168bd989fd317a0ee1e194de392
Hypnosss <398906857@qq.com>
2019-07-22 22:26:05 +0800

    first commit

--- miao/lodash/hypnosss-lodash.js
+++ miao/lodash/hypnosss-lodash.js
@@ -1430,7 +1430,8 @@ var hypnosss = {
     }
     return i == 0;
   },
-  isEqual: function(val1, val2) {
+  isEqual: function a(val1, val2) {
+    console.log(val1, Object.prototype.toString.call(val1))
     var len1 = 0, len2 = 0;
     for(key in val1) {
       len1++;
